f493b0b8675d773a10c7f7f20561d887c4e1d1ab
Giposfila <ozipov26@yandex.ru>
2025-11-16 19:00:45 +0700

    раскрывающаяся списки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionHeaders = document.querySelectorAll('.accordion__header');
+
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+      const accordionItem = header.parentElement;
+      accordionItem.classList.toggle('active');
+    });
+  });
+});
+
